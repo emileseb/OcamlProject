5c90d9eedcf22637be92e33eacba99ef70a72d53
emileseb <emile.seb@gmail.com>
2019-11-20 18:55:09 +0100

    ecart_of_flot begined

--- src/fordfulkerson.ml
+++ src/fordfulkerson.ml
@@ -1,37 +1,49 @@
 open Tools
 open Graph
 
-(*A TESTER*)
+(*VALIDEE*)
 (*Instance de tools.gmap qui prépare le graph avec les capacités*)
 let gmap_flot_of_string = 
   let flot_of_string = fun s -> (0, int_of_string s) in
   fun graph -> gmap graph flot_of_string
 ;;
-(*A TESTER*)
+(*VALIDEE*)
 (*le contraire de la fct précédente*)
 let gmap_string_of_flot =
   let string_of_flot = fun f -> match f with
     |(a,b) -> Printf.sprintf "(%d/%d)" a b in 
   fun graph -> gmap graph string_of_flot
 ;;
+
 (*PAS FINI*)
 (* Algo de FordFulkerson*)
-
-let fordfulkerson s=  fun s_graph -> (*string graph -> (int * string graph)*) 
+(*let fordfulkerson s=  fun s_graph source puit-> (*string graph -> (int * string graph)*) 
   (*let f_graph = flot_of_string s_graph in
     let e_graph = ecart_of_flot f_graph in
+    let run = Match find_path e_graph source puit with
+    |None -> (* end *)
+    |Some (chemin, capacite) -> (*incrémenter e_graph*)
+
   *)
   (1,[])
-;;
-(*A TESTER*)
+  ;;*)
+
+(*Pas Fini*)
 (*Créé le graph d'écart à partir du graph de capacité*) 
-let ecart_of_flot =
-  let ecart_of_flot = fun f -> match f with
-    |(a,b) -> b in
-  fun graph -> gmap graph ecart_of_flot
-
+let ecart_of_flot = fun graph ->
+  let aller = fun lb -> match lb with 
+    |(a,b) -> b-a in
+  let retour = fun lb -> match lb with 
+    |(a,b) -> a in
+  let g = clone_nodes graph in
+  let create_arc = fun gr id1 id2 lbl -> match (retour lbl) with (*Il faudrait aussi tester si un arc n'a pas déjà été créé (cas ou il y a une capacité aller et retour) *)
+    | 0 -> new_arc gr id1 id2 (aller lbl) 
+    | x -> new_arc gr id1 id2 (aller lbl) -> new_arc gr id2 id1 x in
+  e_fold graph (fun gr id1 id2 lbl -> create_arc gr id1 id2 lbl) g
 ;;
 
+(*A TESTER*)
+(*trouve un chemin de source à puit*)
 let find_path = fun graph source puit -> 
   let rec find_path2 = fun  g node target visited m ->
     let o = List.find_opt (fun a -> a = node) visited in
--- src/fordfulkerson.mli
+++ src/fordfulkerson.mli
@@ -8,7 +8,7 @@ val gmap_flot_of_string: string graph -> (int * int) graph
 val gmap_string_of_flot: (int * int) graph -> string graph
 
 (* Algo de FordFulkerson*)
-val fordfulkerson: string graph -> (int * string graph)
+(*val fordfulkerson: string graph -> id -> id -> (int * string graph)*)
 
 (*Créé le graph d'écart à partir du graph de capacité*)
 val ecart_of_flot: (int * int) graph -> int graph
@@ -17,4 +17,5 @@ val ecart_of_flot: (int * int) graph -> int graph
 (*val find_better_path: int graph -> (int * int) graph -> (int * (int * int) graph) option*)
 
 (*trouve un chemin de source à puit*)
+(*Graph d'écart -> Source -> Puit -> (chemin trouvé * capacité max du chemin) si trouvé *)
 val find_path: int graph -> id -> id -> (id list * int) option
